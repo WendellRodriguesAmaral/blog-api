f72760510629bc2bd6b0acb821fc66b479ff3ddb
Wendell Rodrigues Amaral <wendellra@ciandt.com>
2024-01-10 21:25:01 -0300

    feat: ajuste CORS

--- src/api/server.js
+++ src/api/server.js
@@ -2,25 +2,30 @@ import { fastify } from "fastify";
 import { DataBaseController } from "./controllers/PostsController.js";
 
 const server = fastify();
+
 const database = new DataBaseController();
 
 server.get("/", (req, res) => {
   return res.status(200).send("Blog API available.");
 });
 
-server.get("/posts", async (req) => {
+server.get("/posts", async (req, res) => {
+  res = setAccessControl(res);
   const search = req.query.search;
   const videos = search ? await database.listBySearch(search) : await database.list();
   return videos;
 });
 
-server.get("/posts/:id", async (req) => {
+server.get("/posts/:id", async (req, res) => {
+  res = setAccessControl(res);
   const postId = req.params.id;
   const post = await database.getById(postId);
   return post;
 });
 
 server.post("/posts", async (req, res) => {
+  res = setAccessControl(res);
+
   //create a new post
   const post = req.body;
   await database.create(post);
@@ -29,6 +34,8 @@ server.post("/posts", async (req, res) => {
 });
 
 server.put("/posts/:id", async (req, res) => {
+  res = setAccessControl(res);
+
   //update a post
   const postId = req.params.id;
   const post = req.body;
@@ -39,6 +46,8 @@ server.put("/posts/:id", async (req, res) => {
 });
 
 server.delete("/posts/:id", async (req, res) => {
+  res = setAccessControl(res);
+
   //delete a post
   const postId = req.params.id;
   await database.delete(postId);
@@ -46,12 +55,19 @@ server.delete("/posts/:id", async (req, res) => {
   return res.status(204).send();
 });
 
-server.get("/posts/category/:category", async (req) => {
+server.get("/posts/category/:category", async (req, res) => {
+  res = setAccessControl(res);
+
   const category = req.params.category;
   const postByCategory = await database.listByCategory(category);
 
   return postByCategory;
 });
+
+
+function setAccessControl(res){
+  return res.header("Access-Control-Allow-Origin","*");
+}
 
 server.listen({
   host: "0.0.0.0",
